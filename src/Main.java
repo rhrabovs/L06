43b7fbf3b9d1c2ee2058ca1942fada54c5aba34d
rhrabovs <classo69@gmail.com>
2025-11-06 19:03:03 +0100

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
 import java.util.Random;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Cviceni L06 - pole");
+        int POZICE_RODIC_DITE = 2;
 
         int[] treeHights;  //zalozeni pole []
         treeHights = new int[10]; // java nezna dynamicke pole, vzdy musim udat rozmer
@@ -27,5 +30,15 @@ public class Main {
         for (int oneElements : treeHights){
             System.out.println(oneElements);
         }
+
+        //String[] simpsons = new String[5];
+        String[] simpsons = {"Homer","Marge","Bart","Lisa","Maggie"}; //mozna definice pole
+        System.out.println("POCET SIMPSONU JE "+simpsons.length);
+
+        String[] rodice = Arrays.copyOfRange(simpsons,0,POZICE_RODIC_DITE);
+        System.out.println("rodice jsou "+Arrays.toString(rodice));
+
+        String[] deti = Arrays.copyOfRange(simpsons,POZICE_RODIC_DITE,simpsons.length);
+        System.out.println("deti jsou "+Arrays.toString(deti));
     }
 }
